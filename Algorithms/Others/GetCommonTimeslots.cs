d7b189f62113e2780238030fc5c856b7eb53d768
John Yuan <dongyuan518@outlook.com>
2019-06-02 23:07:30 +0800

    Add other algorithms...

---
+++ Algorithms/Others/GetCommonTimeslots.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Algorithms.Others
+{
+    /// <summary>
+    /// The intention here is to try to find the 3 earliest common timeslots that exist in all of the give timeslot lists
+    /// </summary>
+    public static class GetCommonTimeslots
+    {
+        /// <summary>
+        /// This method will return the ordered available time slots for an given user.
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
+        public static List<string> GetTimeSlots(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<string> GetThreeEarliestTimeSlots(List<string> userNames)
+        {
+            var timeSlotList = new List<List<string>>();
+            userNames.ForEach(u => timeSlotList.Add(GetTimeSlots(u)));
+
+            var counterTable = new Dictionary<string, int>();
+
+            foreach (var timeSlots in timeSlotList)
+            {
+                foreach (var timeSlot in timeSlots)
+                {
+                    if (!counterTable.ContainsKey(timeSlot))
+                    {
+                        counterTable.Add(timeSlot, 1);
+                    }
+
+                    counterTable[timeSlot]++;
+                }
+            }
+
+            return counterTable.Where(c => c.Value == userNames.Count).OrderBy(t => t.Key).Take(3).Select(r => r.Key).ToList();
+        }
+    }
+}
